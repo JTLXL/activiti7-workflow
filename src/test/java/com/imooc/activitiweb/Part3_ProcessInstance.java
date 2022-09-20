fbd77e69189386c63a3ba7deeabafd5e4ea67985
amazin <1096060513@qq.com>
2022-09-21 01:41:37 +0800

    update 配置文件

--- src/test/java/com/imooc/activitiweb/Part3_ProcessInstance.java
+++ src/test/java/com/imooc/activitiweb/Part3_ProcessInstance.java
@@ -20,7 +20,7 @@ public class Part3_ProcessInstance {
         //1、获取页面表单填报的内容，请假时间，请假事由，String fromData
         //2、fromData 写入业务表，返回业务表主键ID==businessKey
         //3、把业务数据与Activiti7流程数据关联
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("myProcess_claim","bKey002");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("Process_v1","bKey002");
         System.out.println("流程实例ID："+processInstance.getProcessDefinitionId());
 
     }
@@ -33,7 +33,7 @@ public class Part3_ProcessInstance {
             System.out.println("--------流程实例------");
             System.out.println("ProcessInstanceId："+pi.getProcessInstanceId());
             System.out.println("ProcessDefinitionId："+pi.getProcessDefinitionId());
-            System.out.println("isEnded"+pi.isEnded());
+            System.out.println("isEnded："+pi.isEnded());
             System.out.println("isSuspended："+pi.isSuspended());
 
         }
